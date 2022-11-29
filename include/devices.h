96f6e4dc478e290e455098213f05e320447e2609
SprintKeyz <63497433+SprintKeyz@users.noreply.github.com>
2022-11-28 21:36:36 -0500

    Add all devices

--- include/devices.h
+++ include/devices.h
@@ -4,9 +4,20 @@
 #define _DEVICES_H_
 
 extern pros::Controller master;
+
 extern pros::Motor FL_mtr;
 extern pros::Motor FR_mtr;
 extern pros::Motor BL_mtr;
 extern pros::Motor BR_mtr;
+
+extern pros::Motor_Group flywheel;
+
+extern pros::Motor intake;
+
+extern pros::ADIDigitalOut indexer;
+extern pros::ADIDigitalOut expansion_left;
+extern pros::ADIDigitalOut expansion_right;
+
+extern pros::Imu inertial;
 
 #endif
--- src/devices.cpp
+++ src/devices.cpp
@@ -1,7 +1,27 @@
 #include "devices.h"
+#include "pros/motors.h"
 
+// our controller
 pros::Controller master = pros::Controller(pros::E_CONTROLLER_MASTER);
-pros::Motor FL_mtr = pros::Motor(4, true);
-pros::Motor FR_mtr = pros::Motor(20, false);
-pros::Motor BL_mtr = pros::Motor(1, true);
-pros::Motor BR_mtr = pros::Motor(2, false);
+
+// chassis motors!
+pros::Motor FL_mtr = pros::Motor(4, pros::motor_gearset_e_t::E_MOTOR_GEARSET_18, true);
+pros::Motor FR_mtr = pros::Motor(20, pros::motor_gearset_e_t::E_MOTOR_GEARSET_18, false);
+pros::Motor BL_mtr = pros::Motor(1, pros::motor_gearset_e_t::E_MOTOR_GEARSET_18, true);
+pros::Motor BR_mtr = pros::Motor(2, pros::motor_gearset_e_t::E_MOTOR_GEARSET_18, false);
+
+// flywheel
+pros::Motor flywheel_A = pros::Motor(3, pros::motor_gearset_e_t::E_MOTOR_GEARSET_06, false);
+pros::Motor flywheel_B = pros::Motor(7, pros::motor_gearset_e_t::E_MOTOR_GEARSET_06, true);
+pros::Motor_Group flywheel = pros::Motor_Group({flywheel_A, flywheel_B});
+
+// intake
+pros::Motor intake = pros::Motor(6, pros::motor_gearset_e_t::E_MOTOR_GEARSET_18, true);
+
+// pneumatics
+pros::ADIDigitalOut indexer = pros::ADIDigitalOut('A');
+pros::ADIDigitalOut expansion_left = pros::ADIDigitalOut('B');
+pros::ADIDigitalOut expansion_right = pros::ADIDigitalOut('C');
+
+// inertial
+pros::Imu inertial = pros::Imu(19);
