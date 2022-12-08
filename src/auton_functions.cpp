c35a6f9db73db1ae8cdb884a098d4b1a9d849a09
Tampa Prep Robotics 727B <97551259+terrapinrobotics@users.noreply.github.com>
2022-12-08 17:02:23 -0500

    Fix auton_functions

--- src/auton_functions.cpp
+++ src/auton_functions.cpp
@@ -1,10 +1,13 @@
 #include "main.h"
 #include "autons.h"
 
-void delay_until_target(pros::Motor_Group motor, int target) {
-
+void delay_until_target(pros::Motor_Group* motor, int target) {
+    while (!(((*motor).get_positions()[0] < target + 5) && ((*motor).get_positions()[0] > target - 5))) {
+        pros::delay(20);
+    }
 }
 
 void auton_functions::spin_intake(int units, int velocity) {
-
+    intake.move_relative(units, velocity);
+    delay_until_target(&intake, units);
 }
